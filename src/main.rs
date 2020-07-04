38cc5e888d3b57c062a43578fd8587958b2346f6
Mario Fernandez <mario.fernandez@hceris.com>
2020-07-04 18:31:32 +0200

    basic cli skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate clap;
+
+use serde_derive::Deserialize;
+use std::fs;
+
+#[derive(Deserialize)]
+struct Config {
+    package: Package,
+}
+
+#[derive(Deserialize, Debug)]
+struct Package {
+    authors: Vec<String>,
+    version: String,
+    description: String,
+}
+
+fn config() -> Config {
+    let content = fs::read_to_string("Cargo.toml").unwrap();
+    toml::from_str(&content).unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = config();
+    let matches = clap_app!(terragrate =>
+                            (version: &*cfg.package.version)
+                            (author: &*cfg.package.authors.join(","))
+                            (about: &*cfg.package.description)
+    )
+    .get_matches();
 }
