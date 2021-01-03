fe6fc8025f11808a4d4e540ec4d5f3614c8adb41
Mario Fernandez <mario.fernandez@hceris.com>
2021-01-03 16:13:03 +0100

    fix linter warning

--- src/format_list.rs
+++ src/format_list.rs
@@ -4,7 +4,7 @@ pub fn format_list<T: fmt::Display>(elements: &[T]) -> String {
     elements
         .iter()
         .map(|c| c.to_string())
-        .filter(|s| s != "")
+        .filter(|s| !s.is_empty())
         .collect::<Vec<String>>()
         .join("\n")
 }
